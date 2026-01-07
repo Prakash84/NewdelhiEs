d1467670905fc05f26e583b8a9e164f2d9df1d32
LAPTOP-8C9L3FQT\Jitender chouhan <prakastiwarichs@gmail.com>
2026-01-07 19:13:38 +0530

    production build ready

--- app/escort-service-in-delhi/Servicpage.js
+++ app/escort-service-in-delhi/Servicpage.js
@@ -282,7 +282,7 @@ const pillars = [
   </div>
 </section>
  {/* hOT Service */}
-        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-6 gap-6 p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-6 gap-6 p-6">
       {services2.map((service, index) => (
         <div
           key={index}
